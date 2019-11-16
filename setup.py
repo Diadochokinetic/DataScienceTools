fe87455c88663e2dc38e5a2c083d050558435d04
Fabian Eckstein <diadochokinetic@gmail.com>
2019-11-16 11:37:31 +0100

    update setuo.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='DataScienceTools',
     version='0.1dev',
-    packages=['dstools',],
+    packages=['dstools','dstools.preprocessing', 'dstools.classifier'],
     license='MIT License',
     long_description=open('README.md').read(),
 )
