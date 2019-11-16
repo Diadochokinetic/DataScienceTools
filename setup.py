044f674ffa986bd9be98ddfa59db4ea81f734e97
Fabian Eckstein <diadochokinetic@gmail.com>
2019-11-16 21:49:03 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     python_requires='>3.6',
     install_requires=[
                     'numpy>=1.16.2',
-                    'pandas>=0.24.2',
+                    'pandas>=0.23.2',
                     'scikit-learn>=0.20.3',
                     'statsmodels>=0.9.0'
                     ]
