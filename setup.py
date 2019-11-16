f7e643e2bbfa4ae60fbe7002ad6c54377606d2d6
Fabian Eckstein <diadochokinetic@gmail.com>
2019-11-16 16:51:49 +0100

    add requirements to setup.py

--- setup.py
+++ setup.py
@@ -6,4 +6,10 @@ setup(
     packages=['dstools','dstools.preprocessing', 'dstools.classifier'],
     license='MIT License',
     long_description=open('README.md').read(),
+    python_requires='>3.6',
+    install_requires=[
+                    'numpy>=1.16.2',
+                    'pandas>=0.24.2',
+                    'scikit-learn>=0.20.3',
+                    ]
 )
