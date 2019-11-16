792750e124d08628975697546a26827bdc9a12aa
Fabian Eckstein <diadochokinetic@gmail.com>
2019-11-16 10:46:25 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='DataScienceTools',
+    version='0.1dev',
+    packages=['dstools',],
+    license='MIT License',
+    long_description=open('README.txt').read(),
+)
