b599797568566e8dfa73255f5bcd9b3d96465542
Fabian Eckstein <diadochokinetic@gmail.com>
2019-11-20 08:49:47 +0100

    remove unused package in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='DataScienceTools',
-    version='0.1dev',
+    version='0.1dev1',
     packages=['dstools',
             'dstools.preprocessing', 
             'dstools.classifier',
